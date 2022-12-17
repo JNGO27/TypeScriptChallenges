c1da3dad4f7a55e9eca61e6ed24c498a6089c6f1
Joseph Ochoa <josephochoa91@gmail.com>
2022-12-17 10:26:09 -0800

    MyPick: Own implementation of the Pick utility type

---
+++ MyPick.ts
@@ -0,0 +1,18 @@
+// // My own implementation of the Pick<T, K> generic
+
+type MyPick<Type, Key extends keyof Type> = {
+  [Prop in Key]: Type[Prop];
+};
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+};
+
+type TodoPreview = Pick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+};
